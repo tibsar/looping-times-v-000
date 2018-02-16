5bbd55ff732ac5d64083094c1616587269083a88
Sara Tibbetts <sxt4866@rit.edu>
2018-02-16 01:16:00 +0000

    automatically backed up by learn

--- spec/times_spec.rb
+++ spec/times_spec.rb
@@ -5,6 +5,8 @@ describe "#using_times" do
 	it "puts out the phrase 'Wingardium Leviosa' 7 times by using the `times` construct" do
 
 	looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
+	
+	binding.pry
 	expect{ using_times }.to output(looping_string).to_stdout 
 	end
 end
